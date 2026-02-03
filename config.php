379884b92c948c01335c39c14e00adb0150e3f5f
0419_Thanapa Piyayodilokchai <ant.thanapa@gmail.com>
2026-02-03 11:34:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Connect to server and select database.
+define('servername', 'localhost');
+define('username', 'root');
+define('password', '');
+define('dbname', 'myhotel_67010419');
+$objCon = mysqli_connect(servername, username, password,dbname);
+?>
